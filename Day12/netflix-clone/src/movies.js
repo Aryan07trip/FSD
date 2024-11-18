ecea3219640be043b9ec2dc0598b99f3154ea382
Aryan07trip <aryantripathy208@gmail.com>
2024-11-18 12:56:37 +0530

    Netflix Clone

---
+++ Day12/netflix-clone/src/movies.js
@@ -0,0 +1,59 @@
+const movies=[
+    {
+            id:1,
+            image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70" ,
+            title:"TARZAN-THE WONDER CAR",
+            language:"Hindi",
+            cost:250,
+    },
+{
+    id:2,
+    image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/d/5/q/2013-blu-ray-eagle-english-gupt-original-imagtxsb28ygkrhy.jpeg?q=70" ,
+    title:"GUPT",
+    language:"Hindi",
+    cost:200,
+},
+{
+    id:3,
+    image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/y/x/l/1985-dvd-eagle-hindi-don-amitabh-bachchan-original-imahfkagscjczgy2.jpeg?q=70" ,
+    title:"DON-Amitabh Bachann",
+    language:"Hindi",
+    cost:150,
+},
+{
+    id:4,
+    image :"https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/w/c/gangs-of-wasseypur-i-original-imadnzhqzxzrgumh.jpeg?q=70" ,
+    title:"Gangs Of Wasseypur I",
+    language:"Hindi",
+    cost:150,
+},
+{
+    id:5,
+    image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/7/u/g/2013-blu-ray-eagle-hindi-anand-original-imagmhmzyz9kqtmt.jpeg?q=70" ,
+    title:"ANAND",
+    language:"Hindi",
+    cost:350,
+},
+{
+    id:6,
+    image :"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/x/w/n/2023-dvd-lionsgate-films-english-john-wick-chapter-4-original-imagrmegfvgfprtz.jpeg?q=70" ,
+    title:"John Wick: Chapter 4",
+    language:"Hindi",
+    cost:150,
+},
+{
+    id:7,
+    image :"https://rukminim2.flixcart.com/image/612/612/av-media/movies/t/h/g/the-deer-hunter-original-imad8c9zmvppbmga.jpeg?q=70" ,
+    title:"The Dear Hunter",
+    language:"Hindi",
+    cost:250,
+},
+{
+    id:8,
+    image :"https://rukminim2.flixcart.com/image/612/612/jeokbrk0/movie/z/6/2/2009-dvd-excel-home-videos-english-avatar-original-imaf3bd6zed7phes.jpeg?q=70" ,
+    title:"AVATAR",
+    language:"Hindi",
+    cost:1000,
+},
+];
+export default movies;
